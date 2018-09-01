55b7b4d7be0f60cfd283a2d5a397f034d9d85dc5
nishanc <inbox.nishan@gmail.com>
2018-09-02 00:55:06 +0530

    JWT Tokens

--- Engage.API/Controllers/AuthController.cs
+++ Engage.API/Controllers/AuthController.cs
@@ -9,6 +9,11 @@ using System.Net.Http.Headers;
 using System.Security.Authentication;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System;
 
 namespace Engage.API.Controllers
 {   
@@ -24,7 +29,7 @@ namespace Engage.API.Controllers
         [AllowAnonymous]
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto){
-            
+
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if(await _repo.UserExists(userForRegisterDto.Username)) 
@@ -41,6 +46,31 @@ namespace Engage.API.Controllers
             var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserForLoginDto userForRegisterDto){
+
+            var userFromRepo = await _repo.Login(userForRegisterDto.Username.ToLower(), userForRegisterDto.Password);
+            if(userFromRepo == null)
+            return Unauthorized();
+
+            //generate token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("super secret key");
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity( new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new {tokenString});
         }
     }
 }
