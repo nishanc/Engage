5e8d91bebec63a8d651e3255d64bc66295f63116
nishanc <inbox.nishan@gmail.com>
2018-09-02 19:54:24 +0530

    Login functionality via SPA

--- Engage.SPA/src/app/nav/nav.component.ts
+++ Engage.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
   login() {
-    console.log(this.model);
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(data => {
+      console.log('logged in successfully');
+    }, error => {
+      console.log('failed to login');
+    });
   }
 }
