17b54ac25387b6cbf3e5a875a40a0c1b298df2cd
nishanc <inbox.nishan@gmail.com>
2018-12-28 15:29:19 +0530

    Upload photo functionality

--- Engage.SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ Engage.SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from '../../_models/Photo';
 import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,15 +11,43 @@ import { FileUploader } from 'ng2-file-upload';
 })
 export class PhotoEditorComponent implements OnInit {
 @Input() photos: Photo[];
-uploader: FileUploader = new FileUploader({});
+uploader: FileUploader;
+baseUrl = environment.apiUrl;
 hasBaseDropZoneOver = false;
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
   }
 
   public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 5 * 1024 * 1024
+    });
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
 }
